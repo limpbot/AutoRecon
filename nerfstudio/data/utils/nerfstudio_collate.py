b150930fb269e8644365a699fda697e421c6aca6
limpbot <leonhardsommer96@gmail.com>
2023-09-25 17:47:30 +0200

    fix bug autorecon

--- nerfstudio/data/utils/nerfstudio_collate.py
+++ nerfstudio/data/utils/nerfstudio_collate.py
@@ -22,7 +22,9 @@ from typing import Callable, Dict, Union
 
 import torch
 import torch.utils.data
-from torch._six import string_classes
+
+#from torch import string_classes
+string_classes = str
 
 from nerfstudio.cameras.cameras import Cameras
 from nerfstudio.utils.images import BasicImages
